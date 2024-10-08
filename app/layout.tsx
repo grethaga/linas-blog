e636b734f377e17202fcf656ca420b814a275399
grethagall <100848664+grethagall@users.noreply.github.com>
2024-10-08 15:47:19 +0200

    add fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,17 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
+import { Cormorant_Garamond, Poppins } from "next/font/google";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+const cormorantGaramond = Cormorant_Garamond({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-cormorant-garamond",
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "600"],
+  variable: "--font-poppins",
 });
 
 export const metadata: Metadata = {
@@ -26,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${cormorantGaramond.variable} ${poppins.variable} bg-neutral-100`}
       >
         {children}
       </body>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        cormorantGaramond: ["var(--font-cormorant-garamond)", "serif"],
+        poppins: ["var(--font-poppins)", "sans-serif"],
       },
     },
   },

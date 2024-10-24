39c4b07b7c6de3885418877666ab4f205540d94b
grethaga <gretha.gall@gmail.com>
2024-10-24 13:49:53 +0200

    add social links icons transform

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -6,14 +6,23 @@ import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 const Socials = () => {
   return (
     <div className="text-gray-600 py-10 text-center">
-      <a href="/">
-        <FontAwesomeIcon icon={faInstagram} className="icon-small px-2" />
+      <a href="/" className="inline-block">
+        <FontAwesomeIcon
+          icon={faInstagram}
+          className="icon-small icon-3d px-2"
+        />
       </a>
-      <a href="/">
-        <FontAwesomeIcon icon={faLinkedin} className="icon-small px-2" />
+      <a href="/" className="inline-block">
+        <FontAwesomeIcon
+          icon={faLinkedin}
+          className="icon-small icon-3d px-2"
+        />
       </a>
-      <a href="/">
-        <FontAwesomeIcon icon={faEnvelope} className="icon-small px-2" />
+      <a href="/" className="inline-block">
+        <FontAwesomeIcon
+          icon={faEnvelope}
+          className="icon-small icon-3d px-2 "
+        />
       </a>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,7 @@ const config: Config = {
       colors: {
         customColor: "#FF9966",
       },
+      transform: ["hover"],
     },
   },
   plugins: [],

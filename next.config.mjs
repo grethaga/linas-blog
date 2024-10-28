9404e96b466880640f32495e28a42056cc9bd4ff
grethaga <gretha.gall@gmail.com>
2024-10-28 12:51:01 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+// next.config.mjs
+export default {
+  async redirects() {
+    return [
+      {
+        source: "/old-path",
+        destination: "/new-path",
+        permanent: true, // permanent (301) oder temporär (302)
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://external-api.com/:path*", // externe API
+      },
+    ];
+  },
+};

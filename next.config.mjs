66fd82b350278577691a844b27d293c79dc46d9a
grethaga <gretha.gall@gmail.com>
2024-10-28 12:54:42 +0100

    undo change next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-// next.config.mjs
-export default {
-  async redirects() {
-    return [
-      {
-        source: "/old-path",
-        destination: "/new-path",
-        permanent: true, // permanent (301) oder temporär (302)
-      },
-    ];
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "https://external-api.com/:path*", // externe API
-      },
-    ];
-  },
-};
+export default nextConfig;
